4c76845234d613828d15fd5d842976cf90d4bcb1
Emmanueladeyemi123 <96444111+Emmanueladeyemi123@users.noreply.github.com>
2022-04-18 18:45:03 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,94 +1,118 @@
+//Create a reference for canvas 
 canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d");
-nasa_mars_images_array = ["image_1.jpg","image_2.jpeg", "image_3.jpg","image_4.jpg"];
- random_number = Math.floor(Math.random() * 4);
- console.log(random_number);
 
-rover_width = 100;
-rover_height = 90;
-rover_x = 10;
-rover_y = 10;
-rover_image = "rover.png";
+//Give specific height and width to the car image
+greencar_width=75;
+greencar_height=100;
 
-background_image =nasa_mars_images_array[random_number];
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+
+//Set initial position for a car image.
+greencar_x=5;
+greencar_y=225;
 
 function add() {
-    background_imgTag = new Image();//defining a variable with new image
-    background_imgTag.onload = uploadBackground;//setting a function,onloading this variable
-    background_imgTag.src = background_image;//loading the image
+	//upload car, and background images on the canvas.
+	background_imgTag=new Image();
+	background_imgTag.onload=uploadBackground;
+	background_imgTag.src=background_image;
 
-    rover_imgTag = new Image();//defining a variable with new image
-    rover_imgTag.onload = uploadRover;//setting a function,onloading this variable
-    rover_imgTag.src = rover_image;//loading the image
+	greencar_imgTag=new Image();
+	greencar_imgTag.onload=uploadgreencar;
+	greencar_imgTag.src=greencar_image;
 }
 
 function uploadBackground() {
-    ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+	//Define function ‘uploadBackground’
+ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
 }
 
-function uploadRover() {
-    ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
+function uploadgreencar() {
+	//Define function ‘uploadgreencar’.
+
+	ctx.drawImage(greencar_imgTag,greencar_x,greencar_y,greencar_width,greencar_height);
 }
+
 
 window.addEventListener("keydown", my_keydown);
 
-function my_keydown(e) {
-    keyPressed = e.keyCode;
-    console.log(keyPressed);
-
-    if (keyPressed == "37") {
-        left();
-        console.log("You have pressed Left arrow");
-    }
-
-    if (keyPressed == "38") {
-        up();
-        console.log("You have pressed up arrow");
-    }
-
-    if (keyPressed == "39") {
-        right();
-        console.log("You have pressed right arrow");
-    }
-
-    if (keyPressed == "40") {
-        down();
-        console.log("You have pressed down arrow");
-    }
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right"); 
+		}
+		
+		
 }
-function up() {
-    if (rover_y >= 0) 
-    {
-        rover_y = rover_y - 10;
-        console.log("When up arrow is pressed,  x = " + rover_x + " | y = " + rover_y);
-        uploadBackground();
-        uploadRover();
-    }
+
+function up()
+{
+	//Define function to move the car upward
+	if (greencar_y>=0)
+	{
+		greencar_y=greencar_y-10;
+		console.log("when up arrow is pressed, x="+greencar_x+",y="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
 }
-function down() {
-    if (rover_y <= 500) 
-    {
-        rover_y = rover_y + 10;
-        console.log("When down arrow is pressed,  x = " + rover_x + " | y = " + rover_y);
-        uploadBackground();
-        uploadRover();
-    }
+
+function down()
+{
+	//Define funct<ion to move the car downward
+	if (greencar_y<=350)
+	{
+		greencar_y=greencar_y+10;
+		console.log("when up arrow is pressed, x="+greencar_x+",y="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
 }
-function left() {
-    if (rover_x >= 0) 
-    {
-        rover_x = rover_x - 10;
-        console.log("When up arrow is pressed,  x = " + rover_x + " | y = " + rover_y);
-        uploadBackground();
-        uploadRover();
-    }
+
+function left()
+{
+	//Define function to move the car left side
+	if (greencar_x>=0)
+	{
+		greencar_x=greencar_x-10;
+		console.log("when up arrow is pressed, x="+greencar_x+",y="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
 }
-function right() {
-    if (rover_x <= 700) 
-    {
-        rover_x = rover_x + 10;
-        console.log("When up arrow is pressed,  x = " + rover_x + " | y = " + rover_y);
-        uploadBackground();
-        uploadRover();
-    }
+
+function right()
+{
+	//Define function to move the car right side
+	if (greencar_x<=750)
+	{
+		greencar_x=greencar_x+10;
+		console.log("when up arrow is pressed, x="+greencar_x+",y="+greencar_y);
+		uploadBackground();
+		uploadgreencar();
+	}
 }
